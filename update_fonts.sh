5126c82a5d8bcea2b3f55c131f86e9fc8c0e6ae8
Donie Leigh <donie.leigh@gmail.com>
2017-11-26 12:18:00 +0800

    new post, update update_fonts.sh

--- update_fonts.sh
+++ update_fonts.sh
@@ -3,10 +3,11 @@
 hexo clean
 hexo g
 
+find public -name *.css | xargs sed -i '' 's#FZBeiWeiKaiShu.ttf#FZBeiWeiKaiShu-full.ttf#g'
 find public -name "*.html" |xargs sed -i '' "s#/styles/crisp.css#$HOME/Projects/blog/public/styles/crisp.css#g"
 find public -name "*.html"|xargs font-spider
 
-cp -f public/fonts/FZBeiWeiKaiShu.ttf themes/crisp/source/fonts/FZBeiWeiKaiShu.ttf
+cp -f public/fonts/FZBeiWeiKaiShu-full.ttf themes/crisp/source/fonts/FZBeiWeiKaiShu.ttf
 
 hexo clean
 hexo s -g
