e7c596e49341cefa96d1cb0af27f75c1f4867bc1
Donie Leigh <donie.leigh@gmail.com>
2019-10-28 15:08:34 +0800

    改用blairos主题

--- update_fonts.sh
+++ update_fonts.sh
@@ -3,11 +3,17 @@
 hexo clean
 hexo g
 
+# find public -name *.css | xargs sed -i '' 's#FZBeiWeiKaiShu.ttf#FZBeiWeiKaiShu-full.ttf#g'
+# find public -name "*.html" |xargs sed -i '' "s#/styles/crisp.css#$HOME/Projects/blog/public/styles/crisp.css#g"
+# find public -name "*.html"|xargs font-spider
+
+# cp -f public/fonts/FZBeiWeiKaiShu-full.ttf themes/crisp/source/fonts/FZBeiWeiKaiShu.ttf
+
 find public -name *.css | xargs sed -i '' 's#FZBeiWeiKaiShu.ttf#FZBeiWeiKaiShu-full.ttf#g'
-find public -name "*.html" |xargs sed -i '' "s#/styles/crisp.css#$HOME/Projects/blog/public/styles/crisp.css#g"
+find public -name "*.html" |xargs sed -i '' "s#/css/style.css#$HOME/Projects/blog/public/css/style.css#g"
 find public -name "*.html"|xargs font-spider
 
-cp -f public/fonts/FZBeiWeiKaiShu-full.ttf themes/crisp/source/fonts/FZBeiWeiKaiShu.ttf
+cp -f public/webfonts/FZBeiWeiKaiShu-full.ttf themes/blairos/source/webfonts/FZBeiWeiKaiShu.ttf
 
 hexo clean
 hexo s -g
